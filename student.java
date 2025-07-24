0345acec186a5ea887251909c4a887758a77a276
anushka <avinashbhoge100@gmail.com>
2025-07-24 12:12:24 +0530

    this is my first commit

---
+++ student.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class student{
+int data=90;
+public static int age=40;
+public int add()
+{
+int a=50;
+return a;
+}
+public static void main (string args[]){
+student s1.add();
+System.out.println(s1.add());
+System.out.println(s1.age);
+System.out.println("by class name"+ student.age)
+}
+}
